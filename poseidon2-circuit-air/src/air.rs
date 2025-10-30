43c311de524eb88c1cd94a9eee9084c07bda01e6
hratoanina <hratoanina@polygon.technology>
2025-10-30 11:30:20 +0100

    temp

--- poseidon2-circuit-air/src/air.rs
+++ poseidon2-circuit-air/src/air.rs
@@ -283,3 +283,118 @@ impl<
         >(self, builder, local, next);
     }
 }
+
+#[cfg(test)]
+mod test {
+
+    use alloc::vec;
+    use core::array;
+
+    use p3_baby_bear::{
+        BabyBear, GenericPoseidon2LinearLayersBabyBear, default_babybear_poseidon2_16,
+    };
+    use p3_challenger::{HashChallenger, SerializingChallenger32};
+    use p3_circuit::WitnessId;
+    use p3_circuit::ops::MmcsVerifyConfig;
+    use p3_circuit::tables::{MmcsPrivateData, MmcsTrace};
+    use p3_commit::ExtensionMmcs;
+    use p3_field::extension::BinomialExtensionField;
+    use p3_fri::{TwoAdicFriPcs, create_benchmark_fri_params};
+    use p3_keccak::{Keccak256Hash, KeccakF};
+    use p3_merkle_tree::{MerkleTreeHidingMmcs, MerkleTreeMmcs};
+    use p3_poseidon2_air::RoundConstants;
+    use p3_symmetric::{CompressionFunctionFromHasher, PaddingFreeSponge, SerializingHasher};
+    use p3_uni_stark::{StarkConfig, prove, verify};
+    use rand::rngs::SmallRng;
+    use rand::{Rng, SeedableRng};
+
+    use crate::air::Poseidon2CircuitAir;
+
+    const WIDTH: usize = 16;
+    const D: usize = 4;
+    const RATE: usize = 8;
+    const CAPACITY: usize = 8;
+    const SBOX_DEGREE: u64 = 7;
+    const SBOX_REGISTERS: usize = 1;
+    const HALF_FULL_ROUNDS: usize = 4;
+    const PARTIAL_ROUNDS: usize = 20;
+
+    #[test]
+    fn prove_poseidon2_sponge() -> Result<
+        (),
+        p3_uni_stark::VerificationError<
+            p3_fri::verifier::FriError<
+                p3_merkle_tree::MerkleTreeError,
+                p3_merkle_tree::MerkleTreeError,
+            >,
+        >,
+    > {
+        type Val = BabyBear;
+        type Challenge = BinomialExtensionField<Val, 4>;
+
+        type ByteHash = Keccak256Hash;
+        let byte_hash = ByteHash {};
+
+        type U64Hash = PaddingFreeSponge<KeccakF, 25, 17, 4>;
+        let u64_hash = U64Hash::new(KeccakF {});
+
+        type FieldHash = SerializingHasher<U64Hash>;
+        let field_hash = FieldHash::new(u64_hash);
+
+        type MyCompress = CompressionFunctionFromHasher<U64Hash, 2, 4>;
+        let compress = MyCompress::new(u64_hash);
+
+        // WARNING: DO NOT USE SmallRng in proper applications! Use a real PRNG instead!
+        type ValMmcs = MerkleTreeHidingMmcs<
+            [Val; p3_keccak::VECTOR_LEN],
+            [u64; p3_keccak::VECTOR_LEN],
+            FieldHash,
+            MyCompress,
+            SmallRng,
+            4,
+            4,
+        >;
+        let mut rng = SmallRng::seed_from_u64(1);
+        let constants = RoundConstants::from_rng(&mut rng);
+        let val_mmcs = ValMmcs::new(field_hash, compress, rng);
+
+        type ChallengeMmcs = ExtensionMmcs<Val, Challenge, ValMmcs>;
+        let challenge_mmcs = ChallengeMmcs::new(val_mmcs.clone());
+
+        type Challenger = SerializingChallenger32<Val, HashChallenger<u8, ByteHash, 32>>;
+        let challenger = Challenger::from_hasher(vec![], byte_hash);
+
+        let air: Poseidon2CircuitAir<
+            Val,
+            GenericPoseidon2LinearLayersBabyBear,
+            D,
+            RATE,
+            CAPACITY,
+            WIDTH,
+            SBOX_DEGREE,
+            SBOX_REGISTERS,
+            HALF_FULL_ROUNDS,
+            PARTIAL_ROUNDS,
+        > = Poseidon2CircuitAir::new(constants);
+
+        // Generate random inputs.
+        let mut rng = SmallRng::seed_from_u64(1);
+
+        let fri_params = create_benchmark_fri_params(challenge_mmcs);
+
+        let trace = air.generate_trace_rows(NUM_PERMUTATIONS, fri_params.log_blowup);
+
+        type Dft = p3_dft::Radix2Bowers;
+        let dft = Dft::default();
+
+        type Pcs = TwoAdicFriPcs<Val, Dft, ValMmcs, ChallengeMmcs>;
+        let pcs = Pcs::new(dft, val_mmcs, fri_params);
+
+        type MyConfig = StarkConfig<Pcs, Challenge, Challenger>;
+        let config = MyConfig::new(pcs, challenger);
+
+        let proof = prove(&config, &air, trace, &vec![]);
+
+        verify(&config, &air, &proof, &vec![])
+    }
+}
