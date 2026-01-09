7404f6c46282b3e1f1de178e873b09252ed40e18
Sai Deng <deng.sai@gmail.com>
2026-01-09 09:49:53 -0800

    fix build

--- test-utils/src/lib.rs
+++ test-utils/src/lib.rs
@@ -4,19 +4,21 @@
 /// Keeping this at 3 ensures efficient FRI proving.
 pub const MAX_CONSTRAINT_DEGREE: usize = 3;
 
+pub use {p3_air, p3_batch_stark, p3_field, p3_lookup, p3_matrix, p3_uni_stark};
+
 /// Macro to generate a constraint degree test for an AIR.
 ///
 /// Usage: `assert_air_constraint_degree!(air, "AirName");`
 #[macro_export]
 macro_rules! assert_air_constraint_degree {
     ($air:expr, $air_name:expr) => {{
-        use p3_air::BaseAir;
-        use p3_batch_stark::symbolic::{get_symbolic_constraints, lookup_data_to_expr};
-        use p3_field::PrimeCharacteristicRing;
-        use p3_lookup::logup::LogUpGadget;
-        use p3_lookup::lookup_traits::{AirLookupHandler, Kind, LookupData};
-        use p3_matrix::Matrix;
-        use p3_uni_stark::SymbolicAirBuilder;
+        use $crate::p3_air::BaseAir;
+        use $crate::p3_batch_stark::symbolic::{get_symbolic_constraints, lookup_data_to_expr};
+        use $crate::p3_field::PrimeCharacteristicRing;
+        use $crate::p3_lookup::logup::LogUpGadget;
+        use $crate::p3_lookup::lookup_traits::{AirLookupHandler, Kind, LookupData};
+        use $crate::p3_matrix::Matrix;
+        use $crate::p3_uni_stark::SymbolicAirBuilder;
 
         type F = p3_baby_bear::BabyBear;
         let mut air = $air;
