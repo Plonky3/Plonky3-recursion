70cf712032b72cb03bc5c1d86438c86d61e8609c
Robin Salen <salenrobin@gmail.com>
2026-04-06 12:26:31 +0900

    fix: make stark-common consistent post_lane reduction

--- recursion/src/recursion.rs
+++ recursion/src/recursion.rs
@@ -101,9 +101,14 @@ where
         A: RecursiveAir<Val<SC>, SC::Challenge, LogUpGadget>,
     {
         let num_tables = self.0.proof.opened_values.instances.len();
+        let common_data = self
+            .0
+            .stark_common
+            .as_ref()
+            .unwrap_or_else(|| self.1.common_data());
         RecursionInput::BatchStark {
             proof: &self.0,
-            common_data: self.1.common_data(),
+            common_data,
             table_public_inputs: vec![vec![]; num_tables],
         }
     }
